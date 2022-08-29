9821632381cae49449d9dd33bfd0ebd66a536e9a
mirawalter <111534678+mirawalter@users.noreply.github.com>
2022-08-29 20:57:02 -0300

    Update leiauteNFe.py

--- models/leiauteNFe.py
+++ models/leiauteNFe.py
@@ -28,29 +28,29 @@ class DetPag(models.AbstractModel):
         xsd_type="tPagType", compute='_compute_formapagamentonfe', readonly="True")
 
 def _compute_formapagamentonfe(self):
-    if self.wem_formapagamento = 'dinheiro':
+    if self.wem_formapagamento == 'dinheiro':
         self.nfe40_tPag = 01
-    if self.wem_formapagamento = 'cheque':
+    if self.wem_formapagamento == 'cheque':
         self.nfe40_tPag = 02
-    if self.wem_formapagamento = 'cartaocredito':
+    if self.wem_formapagamento == 'cartaocredito':
         self.nfe40_tPag = 03
-    if self.wem_formapagamento = 'cartaodebito':
+    if self.wem_formapagamento == 'cartaodebito':
         self.nfe40_tPag = 04
-    if self.wem_formapagamento = 'creditoloja':
+    if self.wem_formapagamento == 'creditoloja':
         self.nfe40_tPag = 05
-    if self.wem_formapagamento = 'valealimentacao':
+    if self.wem_formapagamento == 'valealimentacao':
         self.nfe40_tPag = 10
-    if self.wem_formapagamento = 'valerefeicao':
+    if self.wem_formapagamento == 'valerefeicao':
         self.nfe40_tPag = 11
-    if self.wem_formapagamento = 'valepresente':
+    if self.wem_formapagamento == 'valepresente':
         self.nfe40_tPag = 12
-    if self.wem_formapagamento = 'valecombustivel':
+    if self.wem_formapagamento == 'valecombustivel':
         self.nfe40_tPag = 13
-    if self.wem_formapagamento = 'outros':
+    if self.wem_formapagamento == 'outros':
         self.nfe40_tPag = 99
 
 def _compute_avistaouaprazo(self):
-    if self.wem_avistaouaprazo = 'avista':
+    if self.wem_avistaouaprazo == 'avista':
         self.nfe40_indPag = [01]
-    if self.wem_avistaouaprazo = 'aprazo':
+    if self.wem_avistaouaprazo == 'aprazo':
         self.nfe40_indPag = [02]
